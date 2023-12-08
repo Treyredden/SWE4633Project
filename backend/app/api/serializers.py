ed1fc092167397f23be01936512de8805884021a
하민철 <minchulha0703@gmail.com>
2023-12-08 04:33:02 -0500

    Revert due to wrong local git email setup
    Revert "Updated code"
    
    This reverts commit 1b41c265bbf1a1779f7f26dc854f21afc77ed35a.

--- backend/app/api/serializers.py
+++ backend/app/api/serializers.py
@@ -1,22 +1,10 @@
 from rest_framework import serializers
 from .models import Cart, Order, Product, User, Payment, CartItem, OrderItem
 
-class CartItemSerializer(serializers.ModelSerializer):
-    product_id = serializers.ReadOnlyField(source='product.product_id')
-    product_name = serializers.ReadOnlyField(source='product.product_name')
-    product_price = serializers.ReadOnlyField(source='product.price')
-
-    class Meta:
-        model = CartItem
-        fields = ['cart_item_id', 'product_id', 'product_name', 'product_price', 'quantity']
-
-
 class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True, read_only=True, source='cartitem_set')
-
     class Meta:
         model = Cart
-        fields = ['cart_id','created_at', 'items']
+        fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,7 +14,7 @@ class OrderSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['product_id', 'product_name', 'description', 'price', 'image_url'] 
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -43,4 +31,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = '__all__'
 
-
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
--- frontend/src/pages/shopping/shopping.jsx
+++ frontend/src/pages/shopping/shopping.jsx
@@ -1,33 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { PRODUCTS } from "../../products"
 import { Product } from "./product"
 import "./shopping.css"
 
 export const Shopping = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        fetch('http://ec2-18-221-168-153.us-east-2.compute.amazonaws.com/api/product/')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => setProducts(data))
-            .catch(error => console.error('Fetch error:', error));
-    }, []);    
-
-    return (
-        <div className='shopping'>
-            <div className="shopTitle">
-                <h1> AWS Grocery Web Application </h1>
-            </div>
-            <div className="products"> 
-                {products.map((product) => { 
-                    console.log(product.id);
-                    return <Product key={product.id} data={product}/>
-                })}
-            </div>
+    return <div className='shopping'>
+        <div className="shopTitle">
+            <h1> AWS Grocery Web Application </h1>
         </div>
-    );
+        <div className="products"> 
+        {" "}
+        {PRODUCTS.map((product) => ( 
+        <Product data={product}/>
+        ))}
+
+        </div>
+    </div>
 }
